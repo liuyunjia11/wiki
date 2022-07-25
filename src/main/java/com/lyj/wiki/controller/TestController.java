a0f18f8d1b5ad9a80baddf9bd225d7a25ce943f0
yjl <liuyunjiauestc@163.com>
2022-07-25 16:42:12 +0800

    2.11 ingrate hot deployment

--- src/main/java/com/lyj/wiki/controller/TestController.java
+++ src/main/java/com/lyj/wiki/controller/TestController.java
@@ -1,14 +1,18 @@
 package com.lyj.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello}")
+    private  String testHello;
     @GetMapping ("/hello")
     public String hello(){
-        return "hello world";
+
+        return "hello world" + testHello;
     }
 
     @PostMapping("/hello/post")
